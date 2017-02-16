9b87e8053e330e98e5f52f742e8311aa5a369db6
serii <seriiburduja@gmail.com>
2017-02-16 02:59:55 +0200

    finish header and footer

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,3 +1,19 @@
-$(function(){
-    
+$(function () {
+    /*choose menu item*/
+    $(document).ready(function () {
+        var location = window.location.href;
+        var cur_url = '/build/' + location.split('/').pop();
+        $('#js-main-menu a').each(function () {
+            var link = $(this).attr('href');
+            if(cur_url == link){
+                $('.js-main-menu__item').removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+    })
+
+    /*show main menu*/
+    $('#js-show-main-menu').on('click', function () {
+       $('#js-main-menu').slideToggle(); 
+    });
 });
